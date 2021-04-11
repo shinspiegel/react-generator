69fdff77ce5cd39d6e1f9c044a6c74bce9d6d7ac
Jeferson 'Shin <shin.jef@gmail.com>
2021-04-11 00:22:32 -0300

    Cleaned the index file to remove old component

--- assets/main/routes.js
+++ assets/main/routes.js
@@ -4,8 +4,6 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 const Router = () => {
     return (
         <BrowserRouter>
-            <Navigation />
-
             <Switch>
                 <Route component={() => <h1>Page</h1>} path='/' exact />
                 <Route component={() => <h1>404</h1>} path='/*' />
